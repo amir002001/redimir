066f8b793a8d1ccb6cabc4bfcda9ac9a4856706f
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2023-07-18 08:53:24 -0400

    feat: protocol code

--- server/main.go
+++ server/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"encoding/binary"
+	"fmt"
 	"log"
 	"net"
+)
+
+const (
+	maxMessageSize uint32 = 4096
+	headerSize     uint32 = 4
 )
 
 func main() {
@@ -25,12 +32,49 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		go processClient(connection)
+
+		if err = processClient(connection); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
 func processClient(connection net.Conn) error {
-	_, err := connection.Write([]byte("hello world"))
-
+	for {
+		if err := oneRequest(connection); err != nil {
+			log.Println(err)
+			break
+		}
+	}
+	err := connection.Close()
 	return err
+}
+
+func oneRequest(connection net.Conn) error {
+	readBuffer := make([]byte, maxMessageSize)
+
+	err := readFull(connection, readBuffer, headerSize)
+	if err != nil {
+		return err
+	}
+
+	len := binary.LittleEndian.Uint32(readBuffer[:4])
+	if len > maxMessageSize {
+		return fmt.Errorf("message too long")
+	}
+
+	err = readFull(connection, readBuffer[headerSize:], len)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Client says: %s\n", string(readBuffer[4:4+len]))
+	return nil
+}
+
+func readFull(connection net.Conn, readBuffer []byte, readN uint32) error {
+	return nil
+}
+
+func writeAll() {
+	return nil
 }
