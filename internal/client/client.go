3e8f4d0ceaad74a5af21460e9d8fe22705b0ecba
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2023-07-29 15:30:37 -0400

    feat: establish connection init

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,22 +1,41 @@
 package client
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+var redimirConnection *net.TCPConn
+
+func init() {
+	remoteAddress := net.TCPAddr{
+		IP:   net.ParseIP("localhost"),
+		Port: 2807, // El Psy Kongroo
+		Zone: "",
+	}
+	conn, err := net.DialTCP("tcp4", nil, &remoteAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+	redimirConnection = conn
+}
 
 func SendParams(params []string) (result string, err error) {
 	switch params[0] {
 	case "get":
 		if len(params) != 2 {
-			return "", fmt.Errorf("wrong usage. Try get <key>")
+			return "", fmt.Errorf("wrong usage. try get <key>")
 		}
 		return handleGet(params[1])
 	case "set":
 		if len(params) != 3 {
-			return "", fmt.Errorf("wrong usage. Try set <key> <new value>")
+			return "", fmt.Errorf("wrong usage. try set <key> <new value>")
 		}
 		return handleSet(params[1], params[2])
 	case "delete":
 		if len(params) != 2 {
-			return "", fmt.Errorf("wrong usage. Try delete <key>")
+			return "", fmt.Errorf("wrong usage. try delete <key>")
 		}
 		return handleDelete(params[1])
 	default:
