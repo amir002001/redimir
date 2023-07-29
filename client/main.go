494e6e22d1adde539cec61c33e487a650c48a319
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2023-07-29 13:41:03 -0400

    feat: client tui

--- client/main.go
+++ client/main.go
@@ -1,56 +1,45 @@
 package main
 
 import (
-	"encoding/binary"
+	"errors"
 	"fmt"
+	"io"
 	"log"
-	"net"
-)
 
-const (
-	headerSize     = 4
-	maxMessageSize = 4096
+	"github.com/knz/bubbline"
 )
 
 func main() {
-	remoteAddr := net.TCPAddr{
-		IP:   net.ParseIP("127.0.0.1"),
-		Port: 3632,
-	}
+	// Instantiate the widget.
+	m := bubbline.New()
 
-	tcpConnection, err := net.DialTCP("tcp4", nil, &remoteAddr)
-	if err != nil {
-		log.Fatal(err)
-	}
+	m.Prompt = "redimir> "
+	for {
+		// Read a line of input using the widget.
+		val, err := m.GetLine()
+		// Handle the end of input.
+		if err != nil {
+			if err == io.EOF {
+				// No more input.
+				break
+			}
+			if errors.Is(err, bubbline.ErrInterrupted) {
+				// Entered Ctrl+C to cancel input.
+				fmt.Println("^C")
+			} else if errors.Is(err, bubbline.ErrTerminated) {
+				fmt.Println("terminated")
+				break
+			} else {
+				fmt.Println("error:", err)
+			}
+			continue
+		}
 
-	if err := query(tcpConnection, "hello1"); err != nil {
-		log.Fatal(err)
+		fmt.Println(val)
+		// Handle regular input.
+		err = m.AddHistory(val)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	if err := query(tcpConnection, "hello2"); err != nil {
-		log.Fatal(err)
-	}
-	if err := query(tcpConnection, "hello3"); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func query(connection *net.TCPConn, message string) error {
-	sendHeader := uint32(len(message))
-	if err := binary.Write(connection, binary.LittleEndian, sendHeader); err != nil {
-		return err
-	}
-	if _, err := connection.Write([]byte(message)); err != nil {
-		return err
-	}
-	var receiveHeader uint32
-	if err := binary.Read(connection, binary.LittleEndian, &receiveHeader); err != nil {
-		return err
-	}
-	if receiveHeader > maxMessageSize {
-		return fmt.Errorf("Message too big")
-	}
-	receiveMessage := make([]byte, receiveHeader)
-	_, err := connection.Read(receiveMessage)
-	fmt.Printf("server says: %s\n", receiveMessage)
-	return err
 }
